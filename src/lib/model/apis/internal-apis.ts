57cd70d795896692d242d0a0738f469e4b81dfa1
Daniel Ibrahimi <Daniel.ibrahimi360@gmail.com>
2026-02-26 14:54:39 +0100

    feat: add api queries to handle crud operation on user profile

--- src/lib/model/apis/internal-apis.ts
+++ src/lib/model/apis/internal-apis.ts
@@ -1,42 +1,51 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { clearUser } from "../slices/auth-slice/authSlice";
+import { ProfileData } from "@/types/profile";
 
 export const internalApi = createApi({
   reducerPath: "internalApi",
   baseQuery: fetchBaseQuery({ baseUrl: "/", credentials: "include" }),
+  tagTypes: ["Profile"],
   endpoints: (builder) => ({
-    getMe: builder.query({
-      query: () => "api/auth/me",
+    getMe: builder.query<{ user: any }, void>({
+      query: () => 'api/member/auth/getme',
     }),
-    getProfile: builder.query({
-      query: () => "api/auth/profile",
+    getProfile: builder.query<ProfileData, void>({
+      query: () => "api/member/account/profile",
+      providesTags: ["Profile"],
     }),
-    getMeMocked: builder.query({
-      query: () => "testServerApi",
+    updateProfile: builder.mutation<ProfileData, Partial<ProfileData>>({
+      query: (profileUpdates) => ({
+        url: "api/member/account/profile",
+        method: "PUT",
+        body: profileUpdates,
+      }),
+      invalidatesTags: ["Profile"], // Automatically triggers getProfile on success
+    }),
+    deleteProfile: builder.mutation<void, void>({
+      query: () => ({
+        url: "api/member/account/profile",
+        method: "DELETE",
+      }),
+      invalidatesTags: ["Profile"],
     }),
     googleLogin: builder.mutation({
       query: (code: string) => ({
-        url: "testServerApi/auth",
+        url: "api/member/auth",
         method: "POST",
         body: { code },
-        headers: {
-          Authorization: `Bearer ${code}`,
-        },
+        headers: { Authorization: `Bearer ${code}` },
       }),
     }),
-    // testing with testServerApi
-    logout: builder.mutation({
-      query: () => ({
-        url: "testServerApi/logout",
-        method: "POST",
-      }),
+    logout: builder.mutation<void, void>({
+      query: () => ({ url: "api/member/logout", method: "POST" }),
       async onQueryStarted(arg, { dispatch, queryFulfilled }) {
         try {
           await queryFulfilled;
           dispatch(clearUser());
           dispatch(internalApi.util.resetApiState());
         } catch (err) {
-          console.log("Logout error:", err);
+          console.error("Logout error:", err);
         }
       },
     }),
@@ -44,9 +53,10 @@ export const internalApi = createApi({
 });
 
 export const {
-  useGetMeQuery,
-  useGetMeMockedQuery,
+  useLazyGetMeQuery,
   useGetProfileQuery,
+  useUpdateProfileMutation,
+  useDeleteProfileMutation,
   useGoogleLoginMutation,
   useLogoutMutation,
 } = internalApi;
